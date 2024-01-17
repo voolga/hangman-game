b00e7f25f1370cf65abb31e0e707fceae319e965
Denis <olga.volotovich@mail.ru>
2024-01-17 20:24:10 +0100

    docs: replace database file

---
+++ questions-db.js
@@ -0,0 +1,39 @@
+const questionList = [
+  { id: 1, word: "planet", hint: "Third from the Sun." },
+  { id: 2, word: "avocado", hint: "Green fruit with a large pit." },
+  { id: 3, word: "kitten", hint: "A small, fluffy domestic pet." },
+  { id: 4, word: "football", hint: "A popular sport with a ball." },
+  {
+    id: 5,
+    word: "library",
+    hint: "A place where books and information are stored.",
+  },
+  { id: 6, word: "guitar", hint: "A musical instrument with six strings." },
+  { id: 7, word: "crocodile", hint: "A large reptile with long jaws." },
+  {
+    id: 8,
+    word: "camera",
+    hint: "A device for taking photographs and videos.",
+  },
+  { id: 9, word: "elephant", hint: "A giant land mammal with a long trunk." },
+  { id: 10, word: "bicycle", hint: "A two-wheeled mode of transportation." },
+  {
+    id: 11,
+    word: "computer",
+    hint: "An electronic device for processing data.",
+  },
+  { id: 12, word: "hamburger", hint: "A popular fast-food sandwich." },
+  {
+    id: 13,
+    word: "penguin",
+    hint: "A flightless bird found in the Southern Hemisphere.",
+  },
+  { id: 14, word: "ocean", hint: "A vast body of saltwater." },
+  { id: 15, word: "volcano", hint: "A mountain that can erupt with lava." },
+  { id: 16, word: "butterfly", hint: "A colorful insect with delicate wings." },
+  {
+    id: 17,
+    word: "rainbow",
+    hint: "A meteorological phenomenon with vibrant colors.",
+  },
+];
